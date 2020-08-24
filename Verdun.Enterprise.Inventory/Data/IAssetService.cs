56f405163d4473084c3e8b7d2c1ab4697ce61872
Jim Chen <jchen@verdunoilco.com>
2020-08-24 13:53:36 -0500

    Check in files to the new Data folder

---
+++ Verdun.Enterprise.Inventory/Data/IAssetService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Verdun.Enterprise.Inventory.Models;
+using Verdun.Enterprise.Inventory.ViewModels;
+
+namespace Verdun.Enterprise.Inventory.Data
+{
+    public interface IAssetService
+    {        
+        IEnumerable<AssetViewModel> Read();
+        //IList<AssetViewModel> GetAssetWithAttribute();
+        IList<AssetViewModel> GetBaseAssets();
+        IEnumerable<InvAssetType> GetAssetTypesWithDefault();
+        //AssetViewModel GetAssetWithAttributeById(int assetId);
+        AssetViewModel GetAssetForEdit(int assetId);
+        IEnumerable<AssetTypeAttributeViewModel> GetAssetAttributesByAssetType(int assetTypeId);
+        IEnumerable<AssetTypeAttributeViewModel> GetAssetAttributeValuesForEdit(int assetId);
+
+        IEnumerable<InvAttributeControlData> GetAttributeControlDataWithDefault(int attributeId);
+        //Dictionary<int, List<InvAttributeControlData>> GetAttributeControlDataWithDefault();
+        void Create(AssetViewModel asset);
+        void Update(AssetViewModel asset);
+        void Destroy(AssetViewModel asset);
+    }
+}
